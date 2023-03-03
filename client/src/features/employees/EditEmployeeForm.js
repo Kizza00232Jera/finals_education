6c74352f06a6d0488fec0ac7ba916355d7ecbebf
kizza <antonio.jera10@gmail.com>
2023-03-03 18:16:30 +0000

    update works but pw must be put  every time

--- client/src/features/employees/EditEmployeeForm.js
+++ client/src/features/employees/EditEmployeeForm.js
@@ -3,8 +3,8 @@ import { useUpdateEmployeeMutation, useDeleteEmployeeMutation } from "./employee
 import { useNavigate } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons"
-import { ROLES } from "../../config/roles"
-import { DEPARTMENTS } from "../../config/departments"
+// import { ROLES } from "../../config/roles"
+// import { DEPARTMENTS } from "../../config/departments"
 
 
 //regex for checking fields
@@ -29,22 +29,23 @@ const EditEmployeeForm = ({employee}) => {
         isError: isDelError,
         error: delerror
     }] = useDeleteEmployeeMutation()
-
+    
     const navigate = useNavigate()
-
+    
     //its same state like in a new user form. 
-    const [employeeName, setUsername] = useState(employee.employeeName)
+    const [employeeName, setName] = useState(employee.employeeName)
     const [validUsername, setValidUsername] = useState(false)
     const [employeeSurname, setSurname] = useState(employee.employeeSurname)
+    const [validSurname, setValidSurname] = useState(false)
     const [employeeEmail, setEmail] = useState(employee.employeeEmail)
-    const [password, setPassword] = useState('')
+    const [employeePassword, setPassword] = useState('')
     const [validPassword, setValidPassword] = useState(false)
-    const [employeeRole, setRoles] = useState(employee.employeeRole)
-    const [employeeDepartment, setDepartment] = useState(employee.employeDepartment)
+    const [employeeRole, setRole] = useState(employee.employeeRole)
+    const [employeeDepartment, setDepartment] = useState(employee.employeeDepartment)
     const [employeeFund, setFund] = useState(employee.employeeFund)
     const [employeeSpent, setSpent] = useState(employee.employeeSpent)
     const [employeeBudget, setBudget] = useState(employee.employeeBudget)
-
+    
     // employee.employeeName = employeeName DONEEE
     // employee.employeeSurname = employeeSurname DONEEE
     // employee.employeeEmail = employeeEmail DONEEE
@@ -55,26 +56,31 @@ const EditEmployeeForm = ({employee}) => {
     // employee.employeeSpent = employeeSpent DONEEE
     // employee.employeeBudget = employeeBudget DONEEE
     
+    
 
-    //checking for valid employeeName and password
+    //checking for valid employeeName, surname and password
     useEffect(() => {
         setValidUsername(EMPLOYEE_REGEX.test(employeeName))
     }, [employeeName])
 
     useEffect(() => {
-        setValidPassword(PWD_REGEX.test(password))
-    }, [password])
+        setValidSurname(EMPLOYEE_REGEX.test(employeeSurname))
+    }, [employeeSurname])
+
+    useEffect(() => {
+        setValidPassword(PWD_REGEX.test(employeePassword))
+    }, [employeePassword])
 
      //checking issuccess and isdellsuccess effects
      useEffect(() => {
-        console.log(isSuccess)
+         console.log(isSuccess)
         if (isSuccess || isDelSuccess) {
-            setUsername('')
+            setName('')
             setSurname('')
             setPassword('')
             setEmail('')
-            setRoles([])
-            setDepartment([])
+            setRole('')
+            setDepartment('')
             setFund('')
             setSpent('')
             setBudget('')
@@ -84,20 +90,22 @@ const EditEmployeeForm = ({employee}) => {
     }, [isSuccess, isDelSuccess, navigate])
 
      //handlers, on usernamechanged, set employeeName to the inputed value
-     const onUsernameChanged = e => setUsername(e.target.value)
+     const onNameChanged = e => setName(e.target.value)
      const onSurnameChanged = e => setSurname(e.target.value)
      const onEmailChanged = e => setEmail(e.target.value)
      const onPasswordChanged = e => setPassword(e.target.value)
      const onFundChanged = e => setFund(e.target.value)
      const onSpentChanged = e => setSpent(e.target.value)
      const onBudgetChanged = e => setBudget(e.target.value)
+    //  const onRolesChanged = e => setRole(e.target.value)
+    //  const onDepartmentsChanged = e => setDepartment(e.target.value)
  
      const onRolesChanged = e => {
          const values = Array.from(
              e.target.selectedOptions,
              (option) => option.value
          )
-         setRoles(values)
+         setRole(values)
      }
  
      const onDepartmentsChanged = e => {
@@ -111,49 +119,53 @@ const EditEmployeeForm = ({employee}) => {
 
     //we want to check if we have an password updated, as well as if we have it without password updated
     const onSaveEmployeeClicked = async (e) => {
-        if (password) {
-            await updateEmployee({ id: employee.id, employeeName, password, employeeRole })
+        if (employeePassword) {
+            await updateEmployee({ id: employee.id, employeeName, employeePassword, employeeRole, employeeSurname, employeeEmail, employeeDepartment, employeeFund, employeeSpent, employeeBudget })
         } else {
-            await updateEmployee({ id: employee.id, employeeName, employeeRole })
+            await updateEmployee({  id: employee.id, employeeName, employeeRole, employeeSurname, employeeEmail, employeeDepartment, employeeFund, employeeSpent, employeeBudget })
         }
     }
+   
+
 
     const onDeleteEmployeeClicked = async () => {
         await deleteEmployee({ id: employee.id })
     }
 
 
-    const options = Object.values(ROLES).map(role => {
-        return (
-            <option
-                key={role}
-                value={role}
+    // const options = Object.values(ROLES).map(role => {
+    //     return (
+    //         <option
+    //             key={role}
+    //             value={role}
 
-            > {role}</option >
-        )
-    })
+    //         > {role}</option >
+    //     )
+    // })
 
-    const options2 = Object.values(DEPARTMENTS).map(department => {
-        return (
-            <option
-                key={department}
-                value={department}
+    // const options2 = Object.values(DEPARTMENTS).map(department => {
+    //     return (
+    //         <option
+    //             key={department}
+    //             value={department}
 
-            > {department}</option>
-        )
-    })
+    //         > {department}</option>
+    //     )
+    // })
+
+    
 
     let canSave
-    if (password) {
-        canSave = [employeeRole.length, validUsername, validPassword].every(Boolean) && !isLoading
+    if (employeePassword) {
+        canSave = [employeeRole, validUsername, validPassword, validSurname, employeeEmail, employeeDepartment.length, employeeFund, employeeSpent, employeeBudget].every(Boolean) && !isLoading
     } else {
-        canSave = [employeeRole.length, validUsername].every(Boolean) && !isLoading
+        canSave = [employeeRole, validUsername, validSurname, employeeEmail, employeeDepartment.length, employeeFund, employeeSpent, employeeBudget].every(Boolean) && !isLoading
     }
 
     const errClass = (isError || isDelError) ? "errmsg" : "offscreen"
-    const validUserClass = !validUsername ? 'form__input--incomplete' : ''
-    const validPwdClass = password && !validPassword ? 'form__input--incomplete' : ''
-    const validRolesClass = !Boolean(employeeRole.length) ? 'form__input--incomplete' : ''
+ 
+
+
 
     const errContent = (error?.data?.message || delerror?.data?.message) ?? ''
 
@@ -185,18 +197,18 @@ const EditEmployeeForm = ({employee}) => {
                 <label className="form__label" htmlFor="employeeName">
                     Name: <span className="nowrap">[3-20 letters]</span></label>
                 <input
-                    className={`form__input ${validUserClass}`}
+                
                     id="employeeName"
                     name="employeeName"
                     type="text"
                     autoComplete="off"
                     value={employeeName}
-                    onChange={onUsernameChanged}
+                    onChange={onNameChanged}
                 /> 
                 <label className="form__label" htmlFor="employeeSurname">
                 Surname: <span className="nowrap">[3-20 letters]</span></label>
             <input
-                className={`form__input ${validUserClass}`}
+               
                 id="employeeSurname"
                 name="employeeSurname"
                 type="text"
@@ -207,7 +219,7 @@ const EditEmployeeForm = ({employee}) => {
                  <label className="form__label" htmlFor="email">
                     Email: <span className="nowrap">[incl @]</span></label>
                 <input
-                    className={`form__input ${validUserClass}`}
+                  
                     id="email"
                     name="email"
                     type="text"
@@ -219,15 +231,38 @@ const EditEmployeeForm = ({employee}) => {
                 <label className="form__label" htmlFor="password">
                     Password: <span className="nowrap">[empty = no change]</span> <span className="nowrap">[4-12 chars incl. !@#$%]</span></label>
                 <input
-                    className={`form__input ${validPwdClass}`}
+                   
                     id="password"
                     name="password"
                     type="password"
-                    value={password}
+                    value={employeePassword}
                     onChange={onPasswordChanged}
                 />
 
+               
+                <label className="form__label" htmlFor="departments">
+                Departments <span className="nowrap">[Available departments: IOS Android Developer PC]</span></label>
+                <input
+                
+                id="department"
+                name="department"
+                type="text"
+                autoComplete="off"
+                value={employeeDepartment}
+                onChange={onDepartmentsChanged}
+                />
                 <label className="form__label" htmlFor="roles">
+                Roles <span className="nowrap">[Available Roles: Admin Manager Employee]</span></label>
+                <input
+                id="role"
+                name="role"
+                type="text"
+                autoComplete="off"
+                value={employeeRole}
+                onChange={onRolesChanged}
+                />
+
+                {/* <label className="form__label" htmlFor="roles">
                     ASSIGNED DEPARTMENTS:</label>
                 <select
                     id="roles"
@@ -238,10 +273,10 @@ const EditEmployeeForm = ({employee}) => {
                     onChange={onDepartmentsChanged}
                 >
                     {options2}
-                </select>
+                </select> */}
 
 
-                <label className="form__label" htmlFor="roles">
+                {/* <label className="form__label" htmlFor="roles">
                     ASSIGNED ROLES:</label>
                 <select
                     id="roles"
@@ -253,7 +288,7 @@ const EditEmployeeForm = ({employee}) => {
                     onChange={onRolesChanged}
                 >
                     {options}
-                </select>
+                </select> */}
                 <label className="form__label" htmlFor="employeeFund">
                 Fund: <span className="nowrap">[number]</span></label>
             <input  
