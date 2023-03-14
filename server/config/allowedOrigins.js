9819620ff24f7fb2c21c23051e162c2609cc5083
kizza <antonio.jera10@gmail.com>
2023-03-14 06:17:11 +0000

    CORS

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,9 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3500',
+    'https://finals-education-front.vercel.app',
+    'https://finals-education-2h9h.vercel.app',
+    'https://finals-education.vercel.app',
     '93.143.191.242',
     '0.0.0.0/0',
 ]
