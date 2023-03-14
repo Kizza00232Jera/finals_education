5ee0b992f61c975ecee7b0857bb9f33f308180a0
kizza <antonio.jera10@gmail.com>
2023-03-14 07:12:46 +0000

    added allowed origin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     'http://localhost:3500',
     'https://finals-education-front.vercel.app',
     'https://finals-education-2h9h.vercel.app',
+    'https://finalsedu.onrender.com',
     'https://finals-education.vercel.app',
     '93.143.191.242',
     '0.0.0.0/0',
