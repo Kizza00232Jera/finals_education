0c3127b93fe75eaacb0990f0dd35a07d58495aff
kizza <antonio.jera10@gmail.com>
2023-03-14 07:05:40 +0000

    basequery link changed

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -10,7 +10,7 @@ export const store = configureStore({
     },
     middleware: getDefaultMiddleware => 
         getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools: true
+    devTools: false
 })
 
 //enabling things that we can use in queries in users and notes list
